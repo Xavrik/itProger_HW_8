4dd65ce6b4bb3f9609391afebc60f33a76229efa
Xavrik <62742647+Xavrik@users.noreply.github.com>
2021-09-13 17:09:39 +0400

    pizdec

--- src/main/java/com/itproger/itproger_hw_8/Controller.java
+++ src/main/java/com/itproger/itproger_hw_8/Controller.java
@@ -5,16 +5,12 @@ import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 
-public class Controller implements Initializable {
+public class Controller  {
 
     @FXML
     private ResourceBundle resources;
@@ -37,18 +33,23 @@ public class Controller implements Initializable {
 
     @FXML
     private Label lb_gram;
-
-
-    @FXML
-    void SetOnKeyReleased(KeyEvent event) {
-        if(numb_in.getCharacters().length() > 0)
-            getnumb(numb_in.getText());
-    }
-
     String num = "";
     float ton = 0;
     float kilo = 0;
     float gram =0;
+
+    @FXML
+    public void SetOnKeyReleased() {
+        if(numb_in.getCharacters().length() > 0) {
+            num = numb_in.getText();
+            System.out.println(num);
+            getWeigth();
+
+
+        }
+
+    }
+
     @FXML
     void initialize() {
         assert numb_in != null : "fx:id=\"numb_in\" was not injected: check your FXML file 'sample.fxml'.";
@@ -56,60 +57,77 @@ public class Controller implements Initializable {
         assert lb_ton != null : "fx:id=\"lb_ton\" was not injected: check your FXML file 'sample.fxml'.";
         assert lb_kilo != null : "fx:id=\"lb_kilo\" was not injected: check your FXML file 'sample.fxml'.";
         assert lb_gram != null : "fx:id=\"lb_gram\" was not injected: check your FXML file 'sample.fxml'.";
-
-    }
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        getWeigth(getnumb(num));
-
-
-
-
-    }
-//Берем данные из поля ввода
-    String getnumb(String numb){
-       numb_in.setOnKeyReleased(value -> num = numb_in.getText() );
-        return numb;
     }
 
-//Список и его умножение
-    void getWeigth ( String numb){
+    //Список и его умножение
+     void getWeigth () {
 
         choice_box.setItems(weigth);
-        choice_box.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            switch (newValue) {
-                case "Тонны":
+            choice_box.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+
+                if (newValue.equals("Тонны")) {
                     ton = Float.parseFloat(num);
                     kilo = Float.parseFloat(num) * 1000;
                     gram = Float.parseFloat(num) * 1000000;
                     lb_ton.setText(String.valueOf(ton));
                     lb_kilo.setText(String.valueOf(kilo));
                     lb_gram.setText(String.valueOf(gram));
-                    break;
-                case "Килограммы":
-                    ton = Float.parseFloat(num) / 0.001f;
-                    kilo = Float.parseFloat(num);
-                    gram = Float.parseFloat(num) * 1000;
+                } else if (newValue.equals("Килограммы")) {
+                    ton = Float.parseFloat(numb_in.getText()) / 0.001f;
+                    kilo = Float.parseFloat(numb_in.getText());
+                    gram = Float.parseFloat(numb_in.getText()) * 1000;
                     lb_ton.setText(String.valueOf(ton));
                     lb_kilo.setText(String.valueOf(kilo));
                     lb_gram.setText(String.valueOf(gram));
-                    break;
-                case "Граммы":
-                    ton = Float.parseFloat(num) / 1000000;
-                    kilo = Float.parseFloat(num) * 0.001f;
-                    gram = Float.parseFloat(num);
+                } else if (newValue.equals("Граммы")) {
+                    ton = Float.parseFloat(numb_in.getText()) / 1000000;
+                    kilo = Float.parseFloat(numb_in.getText()) * 0.001f;
+                    gram = Float.parseFloat(numb_in.getText());
                     lb_ton.setText(String.valueOf(ton));
                     lb_kilo.setText(String.valueOf(kilo));
                     lb_gram.setText(String.valueOf(gram));
-                    break;
-
-            }
+                }
 
 
-        });
 
-    }
+//            switch (newValue) {
+//                case "Тонны":
+//                    ton = Float.parseFloat(num);
+//                    kilo = Float.parseFloat(num) * 1000;
+//                    gram = Float.parseFloat(num) * 1000000;
+//                    lb_ton.setText(String.valueOf(ton));
+//                    lb_kilo.setText(String.valueOf(kilo));
+//                    lb_gram.setText(String.valueOf(gram));
+//                    break;
+//                case "Килограммы":
+//                    ton = Float.parseFloat(numb_in.getText()) / 0.001f;
+//                    kilo = Float.parseFloat(numb_in.getText());
+//                    gram = Float.parseFloat(numb_in.getText()) * 1000;
+//                    lb_ton.setText(String.valueOf(ton));
+//                    lb_kilo.setText(String.valueOf(kilo));
+//                    lb_gram.setText(String.valueOf(gram));
+//                    break;
+//
+//                case "Граммы":
+//                    ton = Float.parseFloat(numb_in.getText()) / 1000000;
+//                    kilo = Float.parseFloat(numb_in.getText()) * 0.001f;
+//                    gram = Float.parseFloat(numb_in.getText());
+//                    lb_ton.setText(String.valueOf(ton));
+//                    lb_kilo.setText(String.valueOf(kilo));
+//                    lb_gram.setText(String.valueOf(gram));
+//                    break;
+//
+//
+//            }
+
+            });
+
+
+
+     }
+
+
+
 
 
 }
